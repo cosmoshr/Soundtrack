5fa034ded307438c49fe7c32b043bdf004e33418
CraftyDH <23249291+CraftyDH@users.noreply.github.com>
2019-06-11 21:27:16 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        name: 'Main Menu',
+        trigger: ['Main Menu', 'Whenever']
+    },
+    {
+        name: 'The Race Begins',
+        trigger: ['Whenever']
+    }
+]
