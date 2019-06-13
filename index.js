87129e60353e2ee1d408cb9f5ba9394ac0c5355e
HomelessET <ehtnotation@gmail.com>
2019-06-13 15:31:53 +1000

    Music

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 export default [
     {
         name: 'Main Menu',
-        trigger: ['Main Menu', 'Whenever']
+        trigger: ['Main Menu']
     },
     {
         name: 'The Race Begins',
+        trigger: ['Whenever' , 'Game Starts']
+    },
+    {
+        name: 'New World',
+        trigger: ['Start Habitation'] 
+    },
+    {
+        name: 'Attacked',
+        trigger: ['Attack']
+    },
+    {
+        name: 'The Mysterious Veil',
+        trigger: ['Loading-Before Main Menu']
+    },
+    {
+        name: 'We are Explorers',
         trigger: ['Whenever']
     }
 ]
