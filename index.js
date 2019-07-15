ec6409e9abdf151baa3950e12fe7d52b522fcf24
CraftyDH <23249291+CraftyDH@users.noreply.github.com>
2019-07-16 09:25:08 +1000

    Add pixi sound (#2)

---
+++ index.js
@@ -0,0 +1,46 @@
+export default [
+    {
+        name: 'Main Menu',
+        trigger: ['Main Menu']
+    },
+    {
+        name: 'The Race Begins',
+        trigger: ['Whenever' , 'Game Starts']
+    },
+    {
+        name: 'New World',
+        trigger: ['Start Habitation'] 
+    },
+    {
+        name: 'Attacked',
+        trigger: ['Attack']
+    },
+    {
+        name: 'The Mysterious Veil',
+        trigger: ['Loading-Before Main Menu']
+    },
+    {
+        name: 'We are Explorers',
+        trigger: ['Whenever']
+    },
+    {
+        name: 'The Beautiful Abyss',
+        trigger: ['Whenever']
+    },
+    {
+        name: 'All the Beautiful Sars'
+        trigger: ['Whenever']
+    },
+    {
+        name: ['The Stars Shine Bright']
+        trigger: ['Whenever']
+    },
+    {
+        name: ['Fly Through The Stars for Solo Cello']
+        trigger: ['Whenever']
+    },
+    {
+        name: ['Into Space']
+        trigger: ['Whenever']
+    
+]
